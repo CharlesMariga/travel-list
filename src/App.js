a9b5a6e8d0dffc0f25fd2bfea3825eab60b356dd
CharlesMariga <charlesmariga37@gmail.com>
2023-07-17 21:50:13 +0300

    feat: calculating statistics as derived state

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -120,10 +120,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (items.length === 0)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (x%)</em>
+      <em>
+        {percentage == 100
+          ? `You got everything! Ready to go ✈️`
+          : `You have ${numItems} items on your list, and you already packed${" "}
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
